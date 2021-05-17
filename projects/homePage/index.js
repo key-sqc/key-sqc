4ef55402d70b7d8cac9ab0f6ebade2fa1265eaee
chestnutBall <2466955952@qq.com>
2021-05-17 15:49:07 +0800

    对于搜索进行l布局l,添加了一些自己常用的一些网站的快捷图标和更换壁纸的功能

--- projects/homePage/index.js
+++ projects/homePage/index.js
@@ -1,19 +1,56 @@
 window.onload = function () {
 	//  搜索的问题
-	let search = document.querySelector('#search_target');
 
-	document.addEventListener('keydown', keydown);
-	
-	function keydown(e) {
-		
-		if (e.keyCode === 13) {
+	(() => {
+		let search = document.querySelector('#search_target');
 
-			//  搜索接口 http://www.baidu.com/s?wd=关键字
+		document.addEventListener('keydown', keydown);
 
-			window.open(`http://www.baidu.com/s?wd=${search.value}`);
+		function keydown(e) {
 
-			//  取消事件
-			document.removeEventListener('keydown', keydown);
+			if (e.keyCode === 13) {
+
+				//  搜索接口 http://www.baidu.com/s?wd=关键字
+
+				window.open(`http://www.baidu.com/s?wd=${search.value}`);
+
+				// 清除输入框
+				search.value = ' ';
+
+				//  取消事件
+				document.removeEventListener('keydown', keydown);
+			}
 		}
-	}
-}
+
+	})();
+
+
+	// 图标加载
+	(() => {
+		let icos = document.querySelectorAll('nav>ul>li');
+
+		icos.forEach(value => {
+			let Dom = value.children;
+			let reg = /^[http:\/\/|https:\/\/]{1}\D{1,}\/$/;
+			let ico = Dom[0].attributes[0].value;
+			if (reg.test(ico)) {
+
+				ico += 'favicon.ico';
+				Dom[0].style.backgroundImage = `url(${ico})`;
+
+			}
+		});
+	})();
+
+
+	// 切换二次元壁纸
+	(() => {
+		let target = document.querySelector('.target');
+		target.onclick = () => {
+			document.querySelector('.bg').children[0].setAttribute('src', 'https://www.qqlykm.cn/api/ACG/api.php');
+			return false;
+		};
+	})();
+
+
+};
